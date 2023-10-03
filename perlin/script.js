0f5568fa86d635f6faad59ee37ad8e15c69c110c
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2023-10-03 17:32:09 +0300

    added controls and made it actually work as intended

--- perlin/script.js
+++ perlin/script.js
@@ -1,11 +1,14 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
-ctx.fillStyle = "rgba(0,0,0)";
-ctx.fillRect(0, 0, c.width, c.height);
-noise.seed(Math.random());
+let stopped = false
 
-let num = 100
-
+document.getElementById("start").addEventListener("click", function () {
+    stopped = false
+    main()
+})
+document.getElementById("stop").addEventListener("click", function () {
+    stopped = true
+})
 function print(x) {console.log(x)}
 
 function normalize(vector) {
@@ -17,64 +20,86 @@ function normalize(vector) {
 }
 
 let grid = [];
-make_grid(0)
-function make_grid(t) {
-    grid = [];
-    for (let i = 0; i < 3600; i++) {
-        let j = Math.floor(i/80);
-        let perlin_noise = noise.perlin3(((i%80+1)/80), ((j+1)/45)*3, t)
-        let perlin_noise_2 = noise.perlin3(((i%80+1)/80), ((j+1)/45)*3, t+1)
 
-        let rands = [perlin_noise_2, perlin_noise];
-        let normalized = normalize(rands);
+
+function noise_grid(t) {
+    grid = [];
+    for (let i = 0; i < 4000; i++) {
+        let j = Math.floor(i/80);
+        let perlin_noise = noise.perlin3(((i%80+1)/80), ((j+1)/45), t)
+
+        let point = [Math.cos(perlin_noise * Math.PI), Math.sin(perlin_noise * Math.PI)];
         // let normalized = [0, -1]
+        grid.push(point);
+    }
+}
+
+function point_grid(X, Y) {
+    grid = [];
+    for (let i = 0; i < 4000; i++) {
+        let j = Math.floor(i/80);
+
+        let point = [(X)-((i%80+1)*16), (Y)-((j+1)*16)]
+        let normalized = normalize(point)
         grid.push(normalized);
     }
 }
-stroke_grid()
 function stroke_grid() {
-    ctx.beginPath()
-    ctx.strokeStyle = "white";
+
     for (let i = 0; i < grid.length; i++) {
         let j = Math.floor(i/80);
-        ctx.moveTo(i%80*16, j*16+8);
-        ctx.lineTo(i%80*16 + grid[i][0]*5, j*16 + 8 + grid[i][1]*5);
+        ctx.beginPath()
+        ctx.strokeStyle = "white";
+        ctx.moveTo(i%80*16+8, j*16+8);
+        ctx.lineTo(i%80*16+8 + grid[i][0]*5, j*16 + 8 + grid[i][1]*5);
+        ctx.stroke();
+
     }
-    ctx.stroke();
+
 }
 
-function Particle(x, y, speed) {
+function color_randomizer() {
+    return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+}
+
+function Particle(x, y, speed, id) {
+    this.id = id
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.movementVector = [0, 0];
+    this.baseColor = color_randomizer()
 
-    this.move = function() {
+    this.move = function(color) {
         const pos = {
             x: this.x,
             y: this.y,
         };
-        let selected_vector = Math.round(pos.x/16) + Math.round(80*((pos.y+8)/16));
-        if (selected_vector >= 3600) {
-            selected_vector = 3599
-        }
-        if (selected_vector < 0) {
-            selected_vector = 0
+
+        this.color = color
+        if (this.color === true) {
+            this.color = this.baseColor
         }
 
-        this.movementVector[0] += grid[selected_vector][0]/16
-        this.movementVector[1] += grid[selected_vector][1]/16
+        let selected_vector = Math.round((pos.x)/16) + 80*Math.round((pos.y)/16);
+        if (selected_vector < 0) {
+            selected_vector += 80
+        }
+
+        this.movementVector[0] += grid[selected_vector][0]/20
+        this.movementVector[1] += grid[selected_vector][1]/20
         this.movementVector = normalize(this.movementVector)
 
 
         ctx.beginPath();
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = this.color;
         ctx.moveTo(pos.x, pos.y);
         let new_pos = [pos.x + this.movementVector[0]*this.speed, pos.y + this.movementVector[1]*this.speed]
         this.x = new_pos[0];
         this.y = new_pos[1];
         ctx.lineTo(new_pos[0], new_pos[1]);
         ctx.stroke();
+
         if (pos.x > c.width) {
             this.x = 0
         }
@@ -88,28 +113,38 @@ function Particle(x, y, speed) {
             this.y = c.height
         }
 
+
     }
 }
-let particles = []
-for (let i = 0; i < num; i++) {
-    let part = new Particle(Math.random()*c.width, Math.random()*c.height, 10);
-    particles.push(part);
-}
-
-let sus = 0;
-function anim() {
-    requestAnimationFrame(anim);
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
-    sus++
-    if (sus%5 === 0) {
+main()
+function main() {
+    ctx.fillStyle = "rgba(0,0,0)";
     ctx.fillRect(0, 0, c.width, c.height);
-    }
-    particles.forEach((particle) => particle.move())
-    // make_grid(sus)
-    if (sus%60 === 0) {
-        make_grid(sus/20)
+    noise.seed(Math.random());
+
+    let num = document.getElementById("number").value
+    let particles = []
+    for (let i = 0; i < num; i++) {
+        let part = new Particle(Math.random()*c.width, Math.random()*c.height, 10 + Math.random(), i);
+        particles.push(part);
     }
 
+    if (document.getElementById("noise").checked) {noise_grid(0)}
+
+    let sus = 0;
+    function anim() {
+        if (!stopped) {requestAnimationFrame(anim);}
+        if (document.getElementById("waves").checked) {noise_grid(sus/120)}
+        if (document.getElementById("center").checked){point_grid(c.width/2, c.height/2)}
+        ctx.fillStyle = "rgba(0,0,0,0.05)";
+        sus++
+        if (sus%5 === 0 && document.getElementById("disappearing").checked) {
+            ctx.fillRect(0, 0, c.width, c.height);
+        }
+        particles.forEach((particle) => particle.move(document.getElementById("randomize").checked? true : (document.getElementById("color").value + (document.getElementById("visibility").checked? "35" : "ff"))))
+
+
+    }
+    anim();
 }
-anim();
 
