10d0c30287f521602f2d5969a938295c6d14ab9f
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2024-08-17 12:12:43 +0300

    added ed selector as well as support for more cards

--- KarutaEd7/script.js
+++ KarutaEd7/script.js
@@ -1,8 +1,12 @@
 
-card = document.querySelector(".card");
-document.body.append(card.cloneNode(true));
+const card = document.querySelector(".card");
+let active_ed = "7";
+document.querySelector(".search_button").addEventListener("click", search);
+for (const element of document.querySelectorAll(".ed_button")) {
+    element.addEventListener("click", change_ed);
 
-document.querySelector(".search_button").addEventListener("click", search)
+}
+
 
 function search() {
     console.log("a")
@@ -11,9 +15,23 @@ function search() {
     console.log(searchVal)
     for (let i = 0; i < data.length; i++) {
         if ((data[i]["character"].toLowerCase().includes(searchVal)) || (data[i]["series"].toLowerCase().includes(searchVal))) {
-            createCard(i)
+            createCard(i);
         }
     }
+}
+
+function change_ed() {
+    document.querySelector(".active").classList.remove("active")
+    this.classList.add("active")
+    active_ed = this.value
+    for (const workingCard of document.querySelectorAll(".card")) {
+        const i = parseInt(workingCard.querySelector(".index").innerText)
+        workingCard.querySelector(".img1").src= data[i]["link1"].replace("%", active_ed);
+        workingCard.querySelector(".img2").src= data[i]["link2"].replace("%", active_ed);
+        workingCard.querySelector(".img3").src= data[i]["link3"].replace("%", active_ed);
+        workingCard.querySelector(".img4").src= data[i]["link4"].replace("%", active_ed);
+    }
+
 }
 
 function removeAllChildNodes(parent) {
@@ -27,8 +45,10 @@ function createCard(i) {
     let workingCard = card.cloneNode(true)
     workingCard.style.display = "grid"
     workingCard.querySelector(".index").innerText = i;
-    workingCard.querySelector(".img1").src= data[i]["link1"].replace("%", "7");
-    workingCard.querySelector(".img2").src= data[i]["link2"].replace("%", "7");
+    workingCard.querySelector(".img1").src= data[i]["link1"].replace("%", active_ed);
+    workingCard.querySelector(".img2").src= data[i]["link2"].replace("%", active_ed);
+    workingCard.querySelector(".img3").src= data[i]["link3"].replace("%", active_ed);
+    workingCard.querySelector(".img4").src= data[i]["link4"].replace("%", active_ed);
     workingCard.querySelector(".name").innerText = data[i]["character"];
     workingCard.querySelector(".series").innerText = data[i]["series"];
 
