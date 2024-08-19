a06474577dd0febc8120ee665041e84a939124ec
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2024-08-19 16:49:52 +0300

    added tags for e3 + fixed tag search being only 1 tag at a time

--- KarutaEd7/script.js
+++ KarutaEd7/script.js
@@ -9,17 +9,23 @@ for (const element of document.querySelectorAll(".ed_button")) {
     element.addEventListener("click", change_ed);
 }
 
+
+
 function search() {
 
     removeAllChildNodes(document.querySelector(".card_container"))
     let searchVal = document.querySelector(".search_bar").value.toLowerCase()
     for (let i = 0; i < data.length; i++) {
-        if ((data[i]["character"].toLowerCase().includes(searchVal)) || (data[i]["series"].toLowerCase().includes(searchVal)) || (data[i]["date"].toLowerCase().includes(searchVal))) {
-            createCard(i);
-        }
+
         if (document.querySelector(".tags").checked) {
-            if (data[i][`${active_ed}tags`].includes(searchVal))
-            createCard(i);
+            if (searchVal.split(" ").every(elem => (data[i][`${active_ed}tags`].includes(elem)))) {
+                createCard(i);
+            }
+        }
+        else {
+            if ((data[i]["character"].toLowerCase().includes(searchVal)) || (data[i]["series"].toLowerCase().includes(searchVal)) || (data[i]["date"].toLowerCase().includes(searchVal))) {
+                createCard(i);
+            }
         }
     }
 }
@@ -28,11 +34,9 @@ function change_ed() {
     document.querySelector(".active").classList.remove("active")
     this.classList.add("active")
     active_ed = this.value
-    for (const workingCard of document.querySelectorAll(".card")) {
-        const i = parseInt(workingCard.querySelector(".index").innerText)
-        workingCard.querySelector(".img1").src= data[i]["link"].replace("%", active_ed);
+    if (document.querySelector(".card")) {
+        search()
     }
-
 }
 
 function removeAllChildNodes(parent) {
@@ -46,7 +50,13 @@ function createCard(i) {
     let workingCard = card.cloneNode(true)
     workingCard.style.display = "grid"
     workingCard.querySelector(".index").innerText = i;
-    workingCard.querySelector(".img1").src= data[i]["link"].replace("%", active_ed);
+    workingCard.querySelector(".img2").src= data[i]["link"].replace("%", active_ed);
+    if (data[i]["link"].includes("versioned")) {
+        workingCard.querySelector(".img1").src= data[i]["link"].replace("%-1", active_ed).replace("/versioned", "");
+    }
+    else {
+        workingCard.querySelector(".img1").remove()
+    }
     workingCard.querySelector(".name").innerText = data[i]["character"];
     workingCard.querySelector(".series").innerText = data[i]["series"];
     workingCard.querySelector(".date").innerText = data[i]["date"];
